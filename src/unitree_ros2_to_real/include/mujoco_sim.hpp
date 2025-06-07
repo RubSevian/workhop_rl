0dc93c7394d67a11a5fd111a1cf92ac82098a32a
RubSevian <ruben.s03@mail.ru>
2025-06-07 12:00:15 +0300

    fix small defect private members in the class field

--- src/unitree_ros2_to_real/include/mujoco_sim.hpp
+++ src/unitree_ros2_to_real/include/mujoco_sim.hpp
@@ -27,6 +27,9 @@ public:
     std::string get_robot_name() const;
     int get_num_motors() const;
 
+private:
+    Agent agent;
+
     enum StateID {
         STATE_INIT,
         STATE_READY
@@ -46,9 +49,6 @@ public:
     const std::vector<int> net2joint_indexes;
     const std::vector<float> stiffness;
     const std::vector<float> damping;
-
-private:
-    Agent agent;
 };
 
 class InterfaceRos : public rclcpp::Node {
