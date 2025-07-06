c8de67f56715819739c4bffe85c40506450a419c
Севьян Рубен Андреевич <rsevyan@npomis.ru>
2025-07-06 15:09:39 +0300

    work on real robot

--- src/unitree_ros2_to_real/include/ros2_rl_go2.hpp
+++ src/unitree_ros2_to_real/include/ros2_rl_go2.hpp
@@ -5,10 +5,10 @@
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_msgs/msg/u_int8_multi_array.hpp>
-// #include <unitree_go/msg/wireless_controller.hpp>
-#include <unitree/idl/go2/WirelessController_.hpp>
-#include <unitree/robot/channel/channel_subscriber.hpp>
-#include <unitree/robot/channel/channel_publisher.hpp>
+#include <unitree_go/msg/wireless_controller.hpp>
+// #include <unitree/idl/go2/WirelessController_.hpp>
+// #include <unitree/robot/channel/channel_subscriber.hpp>
+// #include <unitree/robot/channel/channel_publisher.hpp>
 #include <unitree/robot/go2/robot_state/robot_state_client.hpp>
 #include <unitree_go/msg/low_state.hpp>
 #include <unitree_go/msg/imu_state.hpp>
@@ -24,39 +24,39 @@
 #include <sstream>
 #include <cmath>
 
-using namespace unitree::robot;
-using namespace unitree::common;
-using namespace unitree::robot::go2;
+// using namespace unitree::robot;
+// using namespace unitree::common;
+// using namespace unitree::robot::go2;
 
-// Структура для кнопок джойстика (как xKeySwitchUnion)
-union xKeySwitchUnion {
-    struct {
-        uint8_t R1 : 1;
-        uint8_t L1 : 1;
-        uint8_t start : 1;
-        uint8_t select : 1;
-        uint8_t R2 : 1;
-        uint8_t L2 : 1;
-        uint8_t F1 : 1;
-        uint8_t F2 : 1;
-        uint8_t A : 1;
-        uint8_t B : 1;
-        uint8_t X : 1;
-        uint8_t Y : 1;
-        uint8_t up : 1;
-        uint8_t right : 1;
-        uint8_t down : 1;
-        uint8_t left : 1;
-    } components;
-    uint16_t value;
-};
+// // Структура для кнопок джойстика (как xKeySwitchUnion)
+// union xKeySwitchUnion {
+//     struct {
+//         uint8_t R1 : 1;
+//         uint8_t L1 : 1;
+//         uint8_t start : 1;
+//         uint8_t select : 1;
+//         uint8_t R2 : 1;
+//         uint8_t L2 : 1;
+//         uint8_t F1 : 1;
+//         uint8_t F2 : 1;
+//         uint8_t A : 1;
+//         uint8_t B : 1;
+//         uint8_t X : 1;
+//         uint8_t Y : 1;
+//         uint8_t up : 1;
+//         uint8_t right : 1;
+//         uint8_t down : 1;
+//         uint8_t left : 1;
+//     } components;
+//     uint16_t value;
+// };
 
 class RobotController {
 public:
     RobotController();
     void initializeRL(const std::string& config_path, const std::string& model_path);
     unitree_go::msg::LowCmd update(const unitree_go::msg::LowState& state,
-        const unitree_go::msg::dds_::WirelessController_& joystick , const xKeySwitchUnion& joy);
+        const unitree_go::msg::WirelessController& joystick);
     void initial_positions(const std::array<unitree_go::msg::MotorState, 20>& motor_state);
     void update_dof_state(const std::array<unitree_go::msg::MotorState, 20>& motor_state);
     float jointLinearInterpolation(float initPos, float targetPos, float rate);
@@ -95,7 +95,7 @@ public:
     ~InterfaceRos();
 private:
     void LowStateHandler(const unitree_go::msg::LowState::SharedPtr msg);
-    void JoystickHandler(const void* msg);
+    void JoystickHandler(const unitree_go::msg::WirelessController::SharedPtr msg);
     void timer_callback_cmd();
     void send_command(unitree_go::msg::LowCmd& cmd);
     void init_cmd();
@@ -108,11 +108,11 @@ private:
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub;
     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr motor_state_pub;
     rclcpp::Subscription<unitree_go::msg::LowState>::SharedPtr state_sub;
-    ChannelSubscriberPtr<unitree_go::msg::dds_::WirelessController_> joystick_sub;
-    unitree_go::msg::dds_::WirelessController_ joystick{};
+    rclcpp::Subscription<unitree_go::msg::WirelessController>::SharedPtr joystick_sub;
     unitree_go::msg::LowCmd low_cmd;
     unitree_go::msg::LowState::SharedPtr latest_state;
+    unitree_go::msg::WirelessController joystick;
     RobotController controller;
 
-    xKeySwitchUnion unitree_joy;
+    // xKeySwitchUnion unitree_joy;
 };
